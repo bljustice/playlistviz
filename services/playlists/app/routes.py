11313a95a49f1df22ae9a44c3e5d96a279e2ab6f
Blake Justice <justice.blake17@gmail.com>
2019-04-25 20:23:18 -0500

    VIZ-3: Add logic and endpoint to extract audio features

--- services/playlists/app/routes.py
+++ services/playlists/app/routes.py
@@ -1,7 +1,13 @@
 from app import app
 from flask import jsonify
+from .utils.api_helpers import get_track_ids, get_audio_features
 
 @app.route('/')
 @app.route('/index')
 def index():
     return 'Hello world'
+
+@app.route('/api/v1/user-playlist')
+def user_playlist():
+    track_ids = get_track_ids('spotify', '37i9dQZF1DWXLeA8Omikj7')
+    return jsonify(get_audio_features(track_ids))
--- services/playlists/app/utils/api_helpers.py
+++ services/playlists/app/utils/api_helpers.py
@@ -1,14 +1,34 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+from app import app
 
-def build_auth(client_id, client_secret):
-    credentials_manager = SpotifyClientCredentials(client_id, client_secret)
+def build_auth():
+    key = app.config.get('APP_CLIENT_KEY')
+    secret = app.config.get('APP_CLIENT_SECRET')
+    credentials_manager = SpotifyClientCredentials(key, secret)
     sp = spotipy.Spotify(client_credentials_manager=credentials_manager)
     return sp
 
-def search_albums(sp, album_urn):
+def get_user_playlist(sp, username, playlist_id):
     try:
-        album = sp.album(album_urn)
-        return album
+        playlist_data = sp.user_playlist(username, playlist_id)
+        return playlist_data
     except:
-        return 'Album could not be returned'
+        return {}
+
+def get_audio_features(track_ids=[]):
+    sp = build_auth()
+    try:
+        audio_features = sp.audio_features(track_ids)
+        return audio_features
+    except:
+        return {}
+
+def get_track_ids(username, playlist_id):
+    sp = build_auth()
+    playlist_json = get_user_playlist(sp, username, playlist_id)
+    if playlist_json:
+        track_metadata = playlist_json.get('tracks').get('items')
+        track_ids = map(lambda k: k['track'].get('id'), track_metadata)
+        return list(track_ids)
+    return []
